90eb2bada9946aff3e7d6dee67f7923199c64b93
rebeccamark <rebecca.mark@47deg.com>
2019-08-27 11:52:17 -0700

    Deletign random files

--- src/main/scala/Option.scala
+++ src/main/scala/Option.scala
@@ -65,8 +65,6 @@ sealed trait Option[+A] {
   // lift(f) returns a function which maps None to None and applies f to the contents of Some.
   def lift[A,B](f: A => B): Option[A] => Option[B] = _ map f
 
-
-
 }
 
 case class Some[+A](get: A) extends Option[A]
@@ -76,8 +74,8 @@ object Option {
   // Exercise 4.3
   // combines two Option values using a binary function. If either Option value is None, then the return value is too.
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = a match {
-    case Some(a) => b match {
-      case Some(b) => Some(f(a, b))
+    case Some(as) => b match {
+      case Some(bs) => Some(f(as, bs))
       case None => None
     }
     case None => None
@@ -87,9 +85,6 @@ object Option {
   // combines a list of Options into one Option containing a list of all the Some values in the original list.
   // If the original list contains None even once, the result of the function should be None
 
-  //  def sequenceTradFold[A](as: List[Option[A]]): Option[List[A]] = {
-  //    as.foldRight[Option[List[A]]](Some(Nil))((x ,y )=> map2(x,y)(_::_)) // first underscore is head, second is tail.
-  //  }
   def sequence[A](as: List[Option[A]]): Option[List[A]] = {
     foldRight(as, Some(List()):Option[List[A]]){ (optionA: Option[A], accumulator: Option[List[A]]) =>
       map2(optionA, accumulator)( (a: A, b: List[A]) => Cons(a, b))
@@ -99,30 +94,15 @@ object Option {
   def sequence1[A](as: List[Option[A]]): Option[List[A]] = as match {
     case Nil => Some(Nil)
     case Cons(Some(head), tail) => sequence1(tail).map(Cons(head,_))
-    case Cons(None, tail) => None
+    case Cons(None, _) => None
   }
 
   // Exercise 4.5
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
-    val listOfOptionB = map(a)(i => f(i))
+  // Pro-tip if you have a map and then a sequence, you can generally rewrite it as a traverse.
+  def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] = {
+    val listOfOptionB: List[Option[B]] = map(as)((a : A) => f(a))
     sequence(listOfOptionB)
   }
-
-//  def traverse1[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] = {
-//    foldRight(as, Some(List()): Option[List[B]])((a: A, accumulator: Option[List[B]]) =>
-//      map2(f(a), accumulator)() )
-//  }
-
-//  def traverse1[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] = {
-//    as match {
-//      case Nil => Some(Nil)
-//      case Cons(a: A, tail: List[A]) => f(a) match {
-//        case Some(b: B) => Some(b).map(Cons(b,traverse1(tail)(f))) // nope
-//        case None => None
-//      }
-//    }
-//  }
-
 }
 
 
